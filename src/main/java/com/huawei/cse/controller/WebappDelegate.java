784eabf229b447438a01394bb419b2ae4e6dfaa0
m00200545 <mojieshui@huawei.com>
2019-02-16 09:31:18 +0800

    version to 1.0 and invoke provider

--- src/main/java/com/huawei/cse/controller/WebappDelegate.java
+++ src/main/java/com/huawei/cse/controller/WebappDelegate.java
@@ -1,14 +1,18 @@
 package com.huawei.cse.controller;
 
+import org.apache.servicecomb.provider.springmvc.reference.RestTemplateBuilder;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 
 @Component
 public class WebappDelegate {
 
+    private RestTemplate restTemplate = RestTemplateBuilder.create();
+    
     public String helloworld(String name){
-
+        String hello = restTemplate.getForObject("cse://helloworld/cse/helloworld?name=" + name, String.class);
         // Do Some Magic Here!
-        return name;
+        return hello;
     }
 }
